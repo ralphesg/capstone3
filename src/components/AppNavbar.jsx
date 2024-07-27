43d9bbe2ec349ad82d1138006c9464efb51e76a7
aya.gallarte <aya.gallarte@gmail.com>
2024-07-27 09:33:02 +0800

    modified UI AyaGallarte

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,47 +1,3 @@
-// import { Navbar, Nav, Container } from 'react-bootstrap';
-// import { Link, NavLink } from 'react-router-dom';
-// import { useContext, useEffect } from 'react';
-// import UserContext from '../context/UserContext';
-// import '../style.css';
-
-// export default function AppNavbar() {
-//     const { user } = useContext(UserContext);
-
-//     return (
-//         <Navbar expand="lg" className="nav">
-//             <Container>
-//                 <Navbar.Toggle aria-controls="basic-navbar-nav" className="navToggle" />
-//                 <Navbar.Collapse id="basic-navbar-nav">
-//                     <Nav className="navlink">
-//                         <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-//                         <Nav.Link as={NavLink} to="/products">Products</Nav.Link>
-//                     </Nav>
-//                     <Nav className="navlink">
-//                         {user.id !== null && user.id !== undefined ? (
-//                             user.isAdmin ? (
-//                                 <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
-//                             ) : (
-//                                 <>
-//                                     <Nav.Link as={NavLink} to="/cart">Cart</Nav.Link>
-//                                     <Nav.Link as={NavLink} to="/order">Orders</Nav.Link>
-//                                     <Nav.Link as={NavLink} to="/logout">Logout</Nav.Link>
-//                                 </>
-//                             )
-//                         ) : (
-//                             <>
-//                                 <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
-//                                 <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
-//                             </>
-//                         )}
-//                     </Nav>
-//                 </Navbar.Collapse>
-//             </Container>
-//         </Navbar>
-//     );
-
-// }
-
-
 import React, { useContext, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
@@ -59,8 +15,7 @@ export default function AppNavbar() {
                     <Nav className="me-auto">
                         <Nav.Link as={NavLink} to="/">Home</Nav.Link>
                         <Nav.Link as={NavLink} to="/products">Products</Nav.Link>
-                    </Nav>
-                    <Nav>
+
                         {user && user.id !== null && user.id !== undefined ? (
                             user.isAdmin ? (       
                                 <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
